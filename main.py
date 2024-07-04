8e75229da104c48d6cadd6e8153b60ae66ce7772
allie <allie@homelab41.homelab41.local>
2024-07-04 15:19:46 +0200

    Added functions to sort and print list of dictionaries.

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def main():
     book_path = "books/frankenstein.txt"
     book_content = open_book(book_path)
     book_word_count = word_count(book_content)
-    book_characters = character_count(convert_to_lowercase(book_content))
-    
-    #print(f"There are {book_word_count} words in the book.")
-    #print(convert_to_lowercase(book_content))
-    #print(book_characters)
+    book_character_count = character_count(convert_to_lowercase(book_content))
+    book_list_of_dictonaries = convert_dictonary_to_listOfDictonaries(book_character_count)
 
+    print(f'--- Begin report of {book_path} ---')
+    print(f'{book_word_count} words found in the document', end='\n \n \n')
+    print_letters(book_list_of_dictonaries)
 
 
 def open_book(book_to_open):
@@ -30,7 +30,22 @@ def character_count(characters_to_count):
             counted_characters[character] = 1
     return counted_characters
 
+def convert_dictonary_to_listOfDictonaries(dictionary):
+    list_of_dictionaries = []
+    for item in dictionary:
+        dict = {"Character": item, "Number": dictionary[item]}
+        list_of_dictionaries.append(dict)
+    return list_of_dictionaries
 
+def sort_on(dict):
+    return dict["Number"]
 
+def print_letters(characters_to_print):
+    characters_to_print.sort(reverse=True, key=sort_on)
+    
+    for i in characters_to_print:
+        char = i["Character"]
+        if char.isalpha() == True:
+            print(f'The {i["Character"]} character was found {i["Number"]} times.')
 
 main()
